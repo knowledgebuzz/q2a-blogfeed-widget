03053c56b6bb4d234ab35b033de630508f5d49c3
Knowledge Buzz <85382558+knowledgebuzz@users.noreply.github.com>
2026-03-02 01:55:03 +0300

    Update qa-plugin.php
    Updated author details

--- qa-plugin.php
+++ qa-plugin.php
@@ -2,11 +2,15 @@
 /*
 Plugin Name: Q2A Blog Feed Widget
 Plugin URI: https://github.com/knowledgebuzz/q2a-blogfeed-widget
-Plugin Description: Displays latest WordPress blog posts in Q2A sidebar with caching and admin controls.
+Plugin Description: A lightweight Question2Answer widget that displays the latest WordPress blog posts in the sidebar with built-in caching, customizable settings, and admin configuration controls.
 Plugin Version: 1.0.0
 Plugin Date: 2026-03-01
-Plugin Author: Davis Simiyu Wanyonyi 
+Plugin Author: Davis Simiyu Wanyonyi
+Plugin Author URI: https://unitedafrica.digital/davis
 Plugin License: GPLv2
+Plugin Minimum Question2Answer Version: 1.8
+Plugin Update Check URI: https://github.com/knowledgebuzz/q2a-blogfeed-widget
+Plugin Support URI: https://unitedafrica.digital/uad-plugin
 */
 
 if (!defined('QA_VERSION')) {
